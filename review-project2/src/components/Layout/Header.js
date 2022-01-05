b56349a7958aa62484372503924bb4475324bad2
jwhong <jwhong98@gmail.com>
2022-01-05 14:01:32 -0800

    added all styling

--- review-project2/src/components/Layout/Header.js
+++ review-project2/src/components/Layout/Header.js
@@ -1,15 +1,16 @@
 import React from "react";
 import img from "../../assets/meals.jpg";
 import classes from "./Header.module.css";
+import HeaderCartButton from "./HeaderCartButton";
 
 const Header = (props) => {
   return (
     <React.Fragment>
       <header className={classes.header}>
         <h1>ReactMeals</h1>
-        <button>Cart</button>
+        <HeaderCartButton />
       </header>
-      <div>
+      <div className={classes["main-image"]}>
         <img src={img} alt="meals" />
       </div>
     </React.Fragment>
